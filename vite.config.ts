bca484dc33e975a959e05dc5d0ac1c5cc3f700d8
Lovell Liu <lovelliu@foxmail.com>
2023-07-12 22:57:38 +0800

    feat: add unocss config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import UnoCSS from 'unocss/vite'
 
-// https://vitejs.dev/config/
+const pathResolve = (dir: string) => resolve(process.cwd(), '.', dir)
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: pathResolve('src') },
+      { find: '#', replacement: pathResolve('types') },
+    ],
+  },
 })
